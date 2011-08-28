ff6beb6e6fe46a918a894de15ba194d288d9db60
FrozenCow <frozencow@gmail.com>
2011-08-28 21:23:03 +0200

    Added SendWebMessageServlet as seperate class.

--- src/main/java/org/dynmap/InternalClientUpdateComponent.java
+++ src/main/java/org/dynmap/InternalClientUpdateComponent.java
@@ -1,31 +1,26 @@
 package org.dynmap;
 
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import org.dynmap.Event.Listener;
+import static org.dynmap.JSONUtils.s;
+
 import org.dynmap.servlet.ClientUpdateServlet;
-import org.dynmap.web.HttpField;
-import org.dynmap.web.HttpHandler;
-import org.dynmap.web.HttpMethod;
-import org.dynmap.web.HttpRequest;
-import org.dynmap.web.HttpResponse;
-import org.dynmap.web.HttpStatus;
-import org.dynmap.web.handlers.ClientUpdateHandler;
-import org.dynmap.web.handlers.SendMessageHandler;
+import org.dynmap.servlet.SendWebMessageServlet;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import static org.dynmap.JSONUtils.*;
 
 public class InternalClientUpdateComponent extends ClientUpdateComponent {
 
     public InternalClientUpdateComponent(final DynmapPlugin plugin, final ConfigurationNode configuration) {
         super(plugin, configuration);
+        
+        plugin.addServlet("/up/", new ClientUpdateServlet(plugin));
+        plugin.addServlet("/up/sendwebmessage", new SendWebMessageServlet(plugin));
+        
+        // TODO: Put WebChat related code in another component.
         final Boolean allowwebchat = configuration.getBoolean("allowwebchat", false);
         final Boolean hidewebchatip = configuration.getBoolean("hidewebchatip", false);
         final Boolean trust_client_name = configuration.getBoolean("trustclientname", false);
         final float webchatInterval = configuration.getFloat("webchat-interval", 1);
         final String spammessage = plugin.configuration.getString("spammessage", "You may only chat once every %interval% seconds.");
-
+        
         plugin.events.addListener("buildclientconfiguration", new Event.Listener<JSONObject>() {
             @Override
             public void triggered(JSONObject t) {
@@ -33,59 +28,6 @@ public class InternalClientUpdateComponent extends ClientUpdateComponent {
                 s(t, "webchat-interval", webchatInterval);
             }
         });
-        
-        plugin.addServlet("/up/", new ClientUpdateServlet(plugin));
-        // TODO: Uncomment!
-        //plugin.webServer.handlers.put("/up/", new ClientUpdateHandler(plugin));
-        
-        // TODO: Uncomment!
-        /*plugin.webServer.handlers.put("/up/sendwebmessage", new HttpHandler() {
-            private final JSONParser parser = new JSONParser();
-            private Charset cs_utf8 = Charset.forName("UTF-8");
-            @Override
-            public void handle(String path, HttpRequest request, HttpResponse response) throws Exception {
-                if (!request.method.equals(HttpMethod.Post)) {
-                    response.status = HttpStatus.MethodNotAllowed;
-                    response.fields.put(HttpField.Accept, HttpMethod.Post);
-                    return;
-                }
-
-                InputStreamReader reader = new InputStreamReader(request.body, cs_utf8);
-                JSONObject o = (JSONObject)parser.parse(reader);
-                Object messageTypeObject = o.get("type");
-                Object messageObject = o.get("message");
-                if (!(messageTypeObject instanceof String) || !(messageObject instanceof JSONObject)) {
-                    response.status = HttpStatus.BadRequest;
-                    return;
-                }
-                String messageType = (String)messageTypeObject;
-                JSONObject message = (JSONObject)messageObject;
-
-                Log.info("WebMessage(" + messageType + "):" + message.toJSONString());
-                plugin.events.trigger("webmessage", new WebMessageEvent(messageType, message));
-                
-                response.fields.put(HttpField.ContentLength, "0");
-                response.status = HttpStatus.OK;
-                response.getBody();
-            }
-        });
-        
-        if (allowwebchat) {
-            SendMessageHandler messageHandler = new SendMessageHandler() {{
-                maximumMessageInterval = (int)(webchatInterval * 1000);
-                spamMessage = "\""+spammessage+"\"";
-                hideip = hidewebchatip;
-                this.trustclientname = trust_client_name;
-                onMessageReceived.addListener(new Listener<SendMessageHandler.Message>() {
-                    @Override
-                    public void triggered(Message t) {
-                        webChat(t.name, t.message);
-                    }
-                });
-            }};
-
-            plugin.webServer.handlers.put("/up/sendmessage", messageHandler);
-        }*/
     }
     
     protected void webChat(String name, String message) {
---
+++ src/main/java/org/dynmap/servlet/SendWebMessageServlet.java
@@ -0,0 +1,51 @@
+package org.dynmap.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.dynmap.DynmapPlugin;
+import org.dynmap.Log;
+import org.dynmap.WebMessageEvent;
+import org.dynmap.web.HttpStatus;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+public class SendWebMessageServlet extends HttpServlet {
+    private final JSONParser parser = new JSONParser();
+    private final DynmapPlugin plugin;
+    
+    public SendWebMessageServlet(DynmapPlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        JSONObject o;
+        try {
+            o = (JSONObject)parser.parse(reader);
+        } catch (ParseException e) {
+            resp.sendError(HttpStatus.BadRequest.getCode());
+            return;
+        }
+        Object messageTypeObject = o.get("type");
+        Object messageObject = o.get("message");
+        if (!(messageTypeObject instanceof String) || !(messageObject instanceof JSONObject)) {
+            resp.sendError(HttpStatus.BadRequest.getCode());
+            return;
+        }
+        String messageType = (String)messageTypeObject;
+        JSONObject message = (JSONObject)messageObject;
+
+        Log.info("WebMessage(" + messageType + "):" + message.toJSONString());
+        plugin.events.trigger("webmessage", new WebMessageEvent(messageType, message));
+        plugin.events.trigger("webmessage_"+messageType, new WebMessageEvent(messageType, message));
+    }
+}
